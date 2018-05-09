05ce3418b9c841401fc26b131f870c695fad56b2
Shetty Damon <ShettyD@xmerseyhc.nhs.uk>
2018-05-09 15:12:52 +0100

    Examine Events

---
+++ Examine Events/ExamineEvents.cs
@@ -0,0 +1,25 @@
+﻿using Examine;
+using Examine.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.BusinessLogic;
+using Umbraco.Core;
+
+namespace Umbraco_Internet_Liverpool_CCG.Models
+{
+    public class ExamineEvents : ApplicationEventHandler
+    {
+        public ExamineEvents()
+        {
+            BaseIndexProvider indexer = ExamineManager.Instance.IndexProviderCollection["FrontEndWebsiteIndexer"];
+            indexer.GatheringNodeData += indexer_GatheringNodeData;
+        }
+
+        private void indexer_GatheringNodeData(object sender, IndexingNodeDataEventArgs e)
+        {
+            e.Fields["createDate"] = DateTime.Parse(e.Fields["createDate"]).ToString("yyyyMMdd000000000");
+        }
+    }
+}
